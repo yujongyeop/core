79ecb2371e43811b8c949427e9b3690a7de3acfb
JongYeob <230233@naver.com>
2024-04-03 22:05:56 +0900

    init

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements  MemberService{
+    // 구체화에 의존(DIP 위반)
+    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
