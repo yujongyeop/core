2495486eb8625fa818e2f12b563931504d396e95
JongYeob <230233@naver.com>
2024-04-04 15:53:22 +0900

    Temp Order Service

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,15 @@
+package hello.core.order;
+
+public class Order {
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+}
