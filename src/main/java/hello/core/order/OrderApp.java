e69dfda78f1ee7ae9223d89a30c769ab1e3ce6bb
JongYeob <230233@naver.com>
2024-04-14 14:48:25 +0900

    add XmlAppContext

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -4,18 +4,23 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member memberA = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(memberA);
 
-        Order order = orderService.createOrder(memberId, "ItemA", 10000);
+        Order order = orderService.createOrder(memberId, "ItemA", 20000);
         System.out.println("order =" + order);
         System.out.println("order.calculatePrice =" + order.calculatePrice());
     }
