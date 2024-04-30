fcfa757502c59fd2f6bfa6c4dbfa8279071e2662
Jongyeob <230233@naver.com>
2024-04-30 19:58:23 +0900

    Singleton Problem

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,14 @@
+package hello.core.singleton;
+
+public class StatefulService {
+//    private int price;
+
+    public int order(String name, int price){
+        System.out.println("name = " + name + ", price = " + price);
+        return price; // 문제 발생
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+}
